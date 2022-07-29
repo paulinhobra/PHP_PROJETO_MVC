8486553686cb75380f89f40a296285b847b5238a
Paulo Henrique <paulo.henrique.braguim@outlook.com>
2022-07-29 09:45:50 -0300

    Página Sobre

---
+++ app/controller/SobreController.php
@@ -0,0 +1,17 @@
+<?php
+
+class SobreController{
+
+    public function index(){
+
+        $loader = new \Twig\Loader\FilesystemLoader('app/view');
+        $twig = new \Twig\Environment($loader);
+        $template = $twig->load('sobre.html');
+
+        $conteudo = $template->render();
+
+        echo $conteudo;
+
+    }
+
+}
